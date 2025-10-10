680386a76c518f0fe3e1a6682822dba122b5ef00
zealmoney <chatwithzeal@gmail.com>
2025-10-10 13:43:04 -0500

    first commit

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://videoapp-backend-blond.vercel.app/api/'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'https://videoapp-backend-blond.vercel.app/api'}),
 
     endpoints: builder => ({
         addRegister: builder.mutation({
